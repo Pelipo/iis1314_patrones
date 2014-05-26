4edb545871e2a0be84d6548ddc1e35ec1dc6d3c6
Jose <jose93_rg@hotmail.com>
2014-05-26 21:43:14 +0200

    añadido Patron_Estrategia

---
+++ Patron_Estrategia/src/Zip.java
@@ -0,0 +1,10 @@
+
+public class Zip implements ICompressor{
+
+	@Override
+	public String compress(String data) {
+		// TODO Auto-generated method stub
+		return "ZIP data: -string to compress-";
+	}
+
+}
